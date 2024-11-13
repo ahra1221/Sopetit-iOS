2cba53f4a8198b4d9022f12e1cb2e0be055e2fb4
ahra_ <ahravelop@gmail.com>
2024-11-13 17:44:44 +0900

    [Feat] #161 - 푸시알림 환경 세팅

--- Sopetit-iOS/Sopetit-iOS/Application/AppDelegate.swift
+++ Sopetit-iOS/Sopetit-iOS/Application/AppDelegate.swift
@@ -9,11 +9,13 @@ import UIKit
 
 import KakaoSDKCommon
 import AuthenticationServices
+import Firebase
 import FirebaseCore
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
@@ -34,17 +36,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
         FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().delegate = self
+        let authOptions: UNAuthorizationOptions = [.alert, .badge,. sound]
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { _, _ in }
+        
+        application.registerForRemoteNotifications()
+        Messaging.messaging().delegate = self
         return true
     }
-
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
@@ -53,24 +67,66 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationDidBecomeActive(_ application: UIApplication) {
         if #available(iOS 13.0, *) {
-                let appleIDProvider = ASAuthorizationAppleIDProvider()
+            let appleIDProvider = ASAuthorizationAppleIDProvider()
             appleIDProvider.getCredentialState(forUserID: UserManager.shared.appleUserIdentifier ?? "") { (credentialState, error) in
-                    switch credentialState {
-                    case .authorized:
-                        print("authorized")
-                    case .revoked:
-                        print("revoked")
-                        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-                              let keyWindow = windowScene.windows.first else {
-                            return
-                        }
-                        UserManager.shared.clearAll()
-                        let nav = LoginViewController()
-                        keyWindow.rootViewController = UINavigationController(rootViewController: nav)
-                    default:
-                        print("notFound")
+                switch credentialState {
+                case .authorized:
+                    print("authorized")
+                case .revoked:
+                    print("revoked")
+                    guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                          let keyWindow = windowScene.windows.first else {
+                        return
                     }
+                    UserManager.shared.clearAll()
+                    let nav = LoginViewController()
+                    keyWindow.rootViewController = UINavigationController(rootViewController: nav)
+                default:
+                    print("notFound")
                 }
+            }
         }
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func application(application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner, .badge])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        let applicationState = UIApplication.shared.applicationState
+        if applicationState == .active || applicationState == .inactive || applicationState == .background {
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                  let keyWindow = windowScene.windows.first else {
+                return
+            }
+            let tabVC = TabBarController()
+            keyWindow.rootViewController = UINavigationController(rootViewController: tabVC)
+        }
+        completionHandler()
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    func messaging(_ messaging: Messaging,
+                   didReceiveRegistrationToken fcmToken: String?) {
+        guard let fcmToken =  fcmToken else { return }
+        print("✅✅✅fcmToken✅✅✅")
+        print(fcmToken)
+        print("✅✅✅fcmToken✅✅✅")
+        UserManager.shared.updateFcmToken(fcmToken)
     }
 }

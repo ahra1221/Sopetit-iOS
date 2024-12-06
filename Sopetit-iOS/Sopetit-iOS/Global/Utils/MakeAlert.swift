cbb267d0fe4b61fd7e2df4f8344cb24a9916524e
ahra1221 <lizze011221@gmail.com>
2024-12-06 15:39:22 +0900

    [Feat] #167 - 서버에러의 경우 띄울 alert 구현

--- Sopetit-iOS/Sopetit-iOS/Global/Utils/MakeAlert.swift
+++ Sopetit-iOS/Sopetit-iOS/Global/Utils/MakeAlert.swift
@@ -22,6 +22,19 @@ extension UIViewController {
         self.present(alertViewController, animated: true, completion: nil)
     }
     
+    func makeServerErrorAlert() {
+        makeVibrate()
+        let alertViewController = UIAlertController(
+            title: "네트워크가 원활하지 않아요.",
+            message: "원활한 앱 사용을 위해\n다시 시도 해주세요.",
+            preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default) { _ in
+            self.moveToSplashView()
+        }
+        alertViewController.addAction(okAction)
+        self.present(alertViewController, animated: true, completion: nil)
+    }
+    
     private func moveToLoginView() {
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
               let keyWindow = windowScene.windows.first else {
@@ -29,6 +42,16 @@ extension UIViewController {
         }
         
         let nav = UINavigationController(rootViewController: LoginViewController())
+        keyWindow.rootViewController = nav
+    }
+    
+    private func moveToSplashView() {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let keyWindow = windowScene.windows.first else {
+            return
+        }
+        
+        let nav = UINavigationController(rootViewController: SplashViewController())
         keyWindow.rootViewController = nav
     }
 }
